1f971b23a412299a08142ed80e1b68752434e4cc
Nevin Mital <mital.nevin@gmail.com>
2018-02-16 11:21:05 -0500

    add works

--- rpn.py
+++ rpn.py
@@ -1,7 +1,28 @@
 #!/usr/bin/env python3
 
+def addNums(arg1, arg2):
+	return arg1 + arg2
+
+operators = {
+	'+': addNums
+}
+
 def calculate(arg):
-	pass
+	stack = list()
+	for val in arg.split():
+		try:
+			num = int(val)
+			stack.append(num)
+		except ValueError:
+			operation = operators[val]
+			arg1 = stack.pop()
+			arg2 = stack.pop()
+			result = operation(arg1, arg2)
+			stack.append(result)
+		print(stack)
+	if len(stack) != 1:
+		raise TypeError("Incorrect number of parameters")
+	return stack.pop()
 
 def main():
 	while True:
