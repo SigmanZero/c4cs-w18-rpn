d6eb1537b44dcaf753f05928af2c85ae885546ca
Nevin Mital <mital.nevin@gmail.com>
2018-02-16 11:11:23 -0500

    init commit framework for rpn, tests for rpn operations

---
+++ test_rpn.py
@@ -0,0 +1,17 @@
+import unittest
+
+import rpn
+
+class TestOperators(unittest.TestCase):
+	def test_add(self):
+		result = rpn.calculate("1 1 +")
+		self.assertEqual(2, result)
+	def test_subtract(self):
+		result = rpn.calculate("10 5 -")
+		self.assertEqual(5, result)
+	def test_multiply(self):
+		result = rpn.calculate("7 4 *")
+		self.assertEqual(28, result)
+	def test_divide(self):
+		result = rpn.calculate("60 20 /")
+		self.assertEqual(3, result)
